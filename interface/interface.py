f0cae2c28c74175e92b879dd0509db656966430d
nathantippetts <nathan.a.tippetts@gmail.com>
2021-11-14 21:55:03 -0700

    Added Flask web interface.

---
+++ interface/interface.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+from virus_total import VirusTotal
+app = Flask(__name__)
+
+posts = [
+    {
+        "author": "KrebsOnSecurity",
+        "title": "Hoax Email Blast Abused Poor Coding in FBI Website",
+        "content": "The Federal Bureau of Investigation (FBI) confirmed today that its fbi.gov domain name and Internet"
+                   " address were used to blast out thousands of fake emails about a cybercrime investigation. "
+                   "According to an interview with the person who claimed responsibility for the hoax, the spam messages"
+                   " were sent by abusing insecure code in an FBI online portal designed to share information with state"
+                   " and local law enforcement authorities.",
+        "date_posted": "November 13, 2021"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "November 13, 2021"
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts, title="Home")
+
+
+@app.route("/integrations")
+def integrations():
+    return render_template("integrations.html", title="Integrations")
+
+
+@app.route("/reputations")
+def reputations():
+    return render_template("reputation.html", title="Reputation Lookup")
+
+
+@app.route("/reputations/hash")
+def hash_reputation():
+    vt = VirusTotal()
+    vt_hash_dict = vt.vt_hash_reputation()
+    vt_results = vt.vt_parse_info(vt_hash_dict)
+    vt_query = vt.query
+    return render_template("reputation_results.html", title="Reputation Results", vt_hashes=vt_results, vt_query=vt_query)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- virus_total.py
+++ virus_total.py
@@ -1,38 +1,47 @@
 from vault import Vault
-import json
 from virus_total_apis import PublicApi as VirusTotalPublicApi
 
 
-class VirusTotal(Vault):
+class VirusTotal:
     """
     Allows a user to send IoCs for reputation checks against VirusTotal
     """
     def __init__(self):
-        super().__init__()
+        vault = Vault()
+        self.query = ""
         self.app = "virus_total"
         # Tries to fetch the API key, if it's not already there then ask the user to supply it
         try:
-            self.API_KEY = super().get_apikey_from_db(self.app)
+            self.API_KEY = vault.get_apikey_from_db(self.app)
         except:
-            self.API_KEY = super().get_apikey_from_user()
-            encrypted = super().encrypt_key(self.API_KEY)
-            super().store_key(encrypted, self.app)
+            self.API_KEY = vault.get_apikey_from_user()
+            encrypted = vault.encrypt_key(self.API_KEY)
+            vault.store_key(encrypted, self.app)
         finally:
             self.vt = VirusTotalPublicApi(self.API_KEY)
 
     def vt_hash_reputation(self):
         # Takes a user entered hash (doesn't matter type) and sends it to virus total
-        query = input("Enter a hash: ")
-        response = self.vt.get_file_report(query)
-        print(json.dumps(response, sort_keys=False, indent=4))
+        # TODO: Figure out how to read in from the web interface
+        self.query = input("Enter a hash: ")
+        response = self.vt.get_file_report(self.query)
+        return response
 
     def vt_url_reputation(self):
         # Takes a user entered URL and sends it to virus total
-        query = input("Enter a URL: ")
-        response = self.vt.get_url_report(query)
-        print(json.dumps(response, sort_keys=False, indent=4))
+        # TODO: Figure out how to read in from the web interface
+        self.query = input("Enter a URL: ")
+        response = self.vt.get_url_report(self.query)
+        return response
 
-    def vt_parse_info(self):
-        # TODO: Parse important info from JSON output for easy viewing
-        pass
-
+    def vt_parse_info(self, response):
+        # Parses out the scan results into a dictionary
+        hashes = []
+        engines = []
+        for v in response.values():
+            if type(v) is dict:
+                for j,x in list(v["scans"].items()):
+                    engines.append(j)
+                    hashes.append(x)
+        results = zip(engines, hashes)
+        return results
